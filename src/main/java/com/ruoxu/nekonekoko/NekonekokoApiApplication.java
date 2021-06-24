aa050bac45fee6f88f62c0b77098042c140b096c
ruoxu <ruoxuz@gmail.com>
2021-06-25 00:22:27 +0900

    Initial commit

---
+++ src/main/java/com/ruoxu/nekonekoko/NekonekokoApiApplication.java
@@ -0,0 +1,13 @@
+package com.ruoxu.nekonekoko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NekonekokoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NekonekokoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ruoxu/nekonekoko/NekonekokoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruoxu.nekonekoko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NekonekokoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
