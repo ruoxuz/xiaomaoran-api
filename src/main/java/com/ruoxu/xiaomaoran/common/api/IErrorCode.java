70cf4ec157986def8840800c892be3acf1eb7a4e
ruoxu <ruoxuz@gmail.com>
2022-04-18 23:32:43 +0900

    添加common api

---
+++ src/main/java/com/ruoxu/xiaomaoran/common/api/IErrorCode.java
@@ -0,0 +1,13 @@
+package com.ruoxu.xiaomaoran.common.api;
+
+public interface IErrorCode {
+    /**
+     * 返回码
+     */
+    long getCode();
+
+    /**
+     * 返回信息
+     */
+    String getMessage();
+}
