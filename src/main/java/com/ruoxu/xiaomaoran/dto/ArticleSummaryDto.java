fb3e8584175a7705a61b233b04e70e7d311a3364
ruoxu <ruoxuz@gmail.com>
2022-04-22 01:59:18 +0900

    添加配置文件

--- src/main/java/com/ruoxu/xiaomaoran/dto/ArticleSummaryDto.java
+++ src/main/java/com/ruoxu/xiaomaoran/dto/ArticleSummaryDto.java
@@ -1,69 +1,26 @@
 package com.ruoxu.xiaomaoran.dto;
 
+import lombok.Data;
+
 import java.util.Date;
 import java.util.Set;
 
 /**
  * @author ruoxu
  */
+@Data
 public class ArticleSummaryDto {
     private String uuid;
 
     private String title;
 
-    private String summary;
+    private String content;
 
-    private Set<AttributeDto> tags;
+    private Boolean personal;
+
+    private Set<AttributeDto> attributes;
 
     private Date createTime;
 
     private Date updateTime;
-
-    public String getUuid() {
-        return uuid;
-    }
-
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getSummary() {
-        return summary;
-    }
-
-    public void setSummary(String summary) {
-        this.summary = summary;
-    }
-
-    public Set<AttributeDto> getTags() {
-        return tags;
-    }
-
-    public void setTags(Set<AttributeDto> tags) {
-        this.tags = tags;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
 }
